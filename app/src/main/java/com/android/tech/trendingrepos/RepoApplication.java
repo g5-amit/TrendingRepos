3dcb17c2fcab35b68d439d0f5122052d7726d6b2
amitgupta <amit9.gupta@paytm.com>
2019-10-06 18:12:13 +0530

    - Dependencies Added and setup in Gradle - Dagger Used for RepoApplication Class - Android Manifest Initial Set up : 2nd Commit

---
+++ app/src/main/java/com/android/tech/trendingrepos/RepoApplication.java
@@ -0,0 +1,25 @@
+package com.android.tech.trendingrepos;
+
+import com.android.tech.trendingrepos.di.component.AppComponent;
+import com.android.tech.trendingrepos.di.component.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+
+/**
+ * We create a custom Application class that extends  {@link dagger.android.DaggerApplication}.
+ * We then override applicationInjector() which tells Dagger how to make our @AppScoped Component
+ * We never have to call `component.inject(this)` as {@link dagger.android.DaggerApplication} will do that for us.
+ */
+public class RepoApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        AppComponent appComponent = DaggerAppComponent.builder().application(this).build();
+        appComponent.inject(this);
+        return appComponent;
+    }
+
+}
+
---
+++ app/src/main/java/com/android/tech/trendingrepos/di/scopes/ActivityScoped.java
@@ -0,0 +1,22 @@
+package com.android.tech.trendingrepos.di.scopes;
+
+
+import com.android.tech.trendingrepos.di.component.AppComponent;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * In Dagger, an unscoped component cannot depend on a scoped component. As
+ * {@link AppComponent} is a scoped component @AppScoped, we create a custom
+ * scope to be used by all fragment components. Additionally, a component with a specific scope
+ * cannot have a sub component with the same scope.
+ */
+@Documented
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScoped {
+}
